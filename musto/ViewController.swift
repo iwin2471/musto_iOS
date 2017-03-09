edc4da2413b258d13d00355d90bfebbe4e76bcc8
Sunrin02 <sunrin02@Sunrin02ui-MacBook-Pro.local>
2017-03-09 22:13:49 +0900

    start project

---
+++ musto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  musto
+//
+//  Created by Sunrin02 on 2017. 3. 9..
+//  Copyright © 2017년 musto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
